4abc1768063e3f278097ca1b36132e09154f4e0a
Nhu Nguyen <nhutnguyen5691@gmail.com>
2021-05-13 12:30:01 -0700

    backoffice/customer entity implemented

--- backoffice_app/src/main/java/com/example/backoffice_app/Customer.java
+++ backoffice_app/src/main/java/com/example/backoffice_app/Customer.java
@@ -7,9 +7,42 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 @Entity
 public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) private Integer id;
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) private Integer id;
+
     private String firstName, lastName;
     private int rewards;
-    private StarbucksCard card;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    } 
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    } 
+
+    public int getRewards() {
+        return this.rewards;
+    }
+
+    public void setTotalRewards(int rewards) {
+        this.rewards = rewards;
+    }
+
+    public void addReward() {
+        this.rewards++;
+    }
+
+    public void subtractReward() {
+        this.rewards--;
+    }
+
+    
 }
