1311e69afd8faa47ba79c44a2b282e7e8abc2d94
Duyen Tran <jasminenguyen49@gmail.com>
2021-05-15 17:54:33 -0700

    Create HomeController.java

---
+++ onlinestore-app/src/main/java/com/example/onlinestoreapp/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.onlinestoreapp;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+       return "starbuckcards";
+    }
+
+}
